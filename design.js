2af8679906c107ece32f8ba9e7d39e28739ece32
techieatkct <108172869+techieatkct@users.noreply.github.com>
2022-07-03 11:44:21 +0530

    Initial commit

--- design.js
+++ design.js
@@ -1,3 +1,21 @@
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
+
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext('2d');
 let hit = new Audio();
